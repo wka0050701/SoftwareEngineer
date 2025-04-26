a7f62016bdb006da29565472bb3d20ba266ec75d
wangkuian1 <wangkuian0701@outlook.com>
2025-04-26 18:09:02 +0800

    first

---
+++ src/main/java/com/example/softwareEngineer/SoftwareEngineerApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwareEngineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareEngineerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softwareEngineer/SoftwareEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwareEngineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
